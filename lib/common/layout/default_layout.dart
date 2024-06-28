bc548fb771988aa0f79d460083bb6be15adef528
heosujinnn <0204sujin@naver.com>
2024-06-28 15:43:33 +0900

    TabBar생성

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -4,14 +4,48 @@ import 'package:flutter/material.dart';
 class DefaultLayout extends StatelessWidget {
   final Widget child;
   final Color? backgroundColor;
+  final String? title;
+  final Widget? bottomNavbar;
 
-  const DefaultLayout({super.key, required this.child, this.backgroundColor});
+  const DefaultLayout({
+    super.key,
+    required this.child,
+    this.backgroundColor,
+    this.title,
+    this.bottomNavbar,
+
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppbar(),
       body: child,
+      bottomNavigationBar: bottomNavbar,
     );
+  }
+
+  AppBar? renderAppbar() {
+    if(title==null){
+      return null;
+    }
+    else{
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Center(
+          child: Text(
+            title!,
+            style: const TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.w500,
+
+            ),
+          ),
+        ),
+        foregroundColor: Colors.black,
+      );
+    }
   }
 }
--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,14 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_practice/common/const/colors.dart';
 import 'package:flutter_practice/common/layout/default_layout.dart';
-class RootTab extends StatelessWidget {
+
+class RootTab extends StatefulWidget {
   const RootTab({super.key});
 
   @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
+  late TabController controller;
+
+  int index = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 4, vsync: this);
+    controller.addListener(tabListener);
+  }
+
+  void tabListener() {
+    setState(() {
+      index = controller.index;
+    });
+  }
+  @override 
+  void dispose() {
+    controller.removeListener(tabListener);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const DefaultLayout(
-      child: Center(
-        child:Text('Root Tab'),
+    return DefaultLayout(
+      title: '수진의 민족',
+      child: TabBarView(
+        physics: NeverScrollableScrollPhysics(),//스와이프 금지
+        controller: controller,
+        children: [
+          Center(child: Container(child: Text('홈'))),
+          Center(child: Container(child: Text('음식'))),
+          Center(child: Container(child: Text('주문'))),
+          Center(child: Container(child: Text('프로필'))),
+        ],
+      ),
+      bottomNavbar: BottomNavigationBar(
+        selectedItemColor: Colors.pinkAccent,
+        unselectedItemColor: BODY_TEXT_COLOR,
+        selectedFontSize: 10,
+        unselectedFontSize: 10,
+        type: BottomNavigationBarType.shifting,
+        // 누를때 확대됨!
+        onTap: (int index) {
+          setState(() {
+            controller.animateTo(index);
+          });
+        },
+        currentIndex: index,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: '홈'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.fastfood_outlined), label: '음식'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.receipt_long_outlined), label: '주문'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline), label: '프로필'),
+        ],
       ),
     );
   }
